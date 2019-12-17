850b6be8a917a6cb0e42ad73fa3cf614dc772956
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2019-12-17 16:44:22 +0100

    add function to extract vector branches

--- Files/convertor.py
+++ Files/convertor.py
@@ -2,11 +2,26 @@
 
 import sys,os,time,stat,logging
 import uproot, pandas
+import numpy as np 
 from optparse import OptionParser
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
+
+def find_vector_branches(in_df):
+    vector_branches_list=[]
+    #x = np.array(in_df.keys()) 
+    #for i in np.unique(x):
+    for i in in_df.keys():
+        #for j in range(len(in_df[i])):
+        for j in range(5):
+            if in_df[i][j].size != 1:
+                vector_branches_list.append(i)
+                break
+
+    return vector_branches_list
+    
 
 def main():
     
@@ -39,7 +54,7 @@ def main():
         log.error("The input path specified doesn't exists.\n\t\t\tPlease, use '-i <path>' to specify an existing input path.")
         sys.exit()
         
-    if not os.path.isfile(inppath+"/"+inpfile):
+    if not os.path.isfile(inppath+"/"+inpfile+".root"):
         log.error("The input file specified doesn't exists in the input path.\n\t\t\tPlease, use '-f <file>' to specify an existing file in the path.")
         sys.exit()
         
@@ -47,11 +62,18 @@ def main():
         log.error("\tThe output path specified doesn't exists.\n\t\t\tPlease, use '-o <path>' to specify an existing output path.")
         sys.exit()
         
+    outname=outpath+'/'+inpfile+'.h5'
     # load input data
-    in_data = uproot.open(inppath+"/"+inpfile)[inpfiletree]
-    #print(in_data.keys())
+    in_data = uproot.open(inppath+"/"+inpfile+".root")[inpfiletree]
+    
     in_data_df = in_data.pandas.df(flatten=False)
-    print(in_data_df.head())
+    del in_data_df['lep_truthParentPdgId_2_new']
+    vector_br_list=find_vector_branches(in_data_df)
+    #vector_list=['jet_flavor_truth_label_ghost','jet_flavor_weight_MV2c10','jet_pT','jet_eta','lepton_PromptLeptonIso_TagWeight','lepton_ChargeIDBDTTight','scale_weights','mcEventWeights','total_weights','electron_ambiguityType','electron_PromptLeptonIso_TagWeight','electron_PromptLeptonVeto_TagWeight','muon_PromptLeptonIso_TagWeight','muon_PromptLeptonVeto_TagWeight']
+    #print(np.array_equal(vector_br_list,vector_list))
+    for i in vector_br_list:
+        del in_data_df[i]
+    in_data_df.to_hdf(outname, key='df', mode='w')
 
 # Start the main routine and log process time
 if __name__ == "__main__":
