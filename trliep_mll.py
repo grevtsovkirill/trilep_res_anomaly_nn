5726a8b4124ef833ab2dc22fb6b23f88a0e32ec3
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-01-03 09:45:11 +0100

    initiate jupnotebook -> py code conversion for production version

---
+++ trliep_mll.py
@@ -0,0 +1,15 @@
+
+import sys,os,time,stat,logging
+import pandas
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    
+    
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    log.info("]\tProcess time: %f" %(time.time() - start))
+
