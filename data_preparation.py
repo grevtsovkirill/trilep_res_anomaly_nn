169fad28ef6422e97e3d163d5d943481a6c5df4b
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-01-06 10:48:37 +0100

    add function for creating lists matching systematics branches, or any additional reuirements

--- data_preparation.py
+++ data_preparation.py
@@ -7,3 +7,16 @@ def apply_3l_Zveto_SF_cuts(df):
     df2=df1.loc[df1.best_Z_Mll>0]    
     return df2
 
+
+def list_branch_to_remove(df,additional_vars=[]):
+    syst_list=['UP','DOWN','CRB','scale','weight','SF','Eff',
+               'forFit','HLT','tau','MV2c20','is1','is2','is3','is4',
+               'ghost','flvWtOrdr','pass']
+    syst_list=syst_list+additional_vars
+    matches_syst=[]
+    features = list(df.columns.values)
+    for x in features:
+        for j in syst_list:
+            if j in x:
+                matches_syst.append(x.strip())
+    return matches_syst
