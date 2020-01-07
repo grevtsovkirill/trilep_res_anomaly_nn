4a62b22d9f606f677c4547be25c57cc500d3560b
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-01-07 13:42:05 +0100

    define randomforest classification model, get the feature ranking

--- trlep_mll.py
+++ trlep_mll.py
@@ -1,9 +1,13 @@
-
 import sys,os,time,stat,logging
 import pandas
 import numpy as np
 import data_preparation
 import plot_helper
+import matplotlib.pyplot as plt
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -19,8 +23,8 @@ def get_data(labels_list):
 
 def main():
     #load data 
-    #labels_list=['ttZ','ttW','ttH','VV','fakes','Others']
-    labels_list=['ttZ']
+    labels_list=['ttZ','ttW','ttH','VV','fakes','Others']
+    #labels_list=['ttZ']
     data, bkg_set=get_data(labels_list)
 
     #apply final selections
@@ -29,10 +33,10 @@ def main():
     
     var='Mll01'
     #test stack plot of whole contributions 
-    plot_helper.plot_stack_var(data_sel,bkg_set_sel,labels_list,'Mll01',0.001)
+    plot_helper.plot_stack_var(data_sel,bkg_set_sel,labels_list,var)
 
     #plot of two classes to find the key feature responsible for the difference
-    plot_helper.ovr_slot(data_sel,bkg_set_sel)
+    plot_helper.ovr_slot(data_sel,bkg_set_sel,var)
     
     #prepare datasets for building the model:
     data_sel_trim = data_sel.drop(data_preparation.list_branch_to_remove(data_sel),axis=1)
@@ -44,6 +48,13 @@ def main():
     #build data for the model:
     X,y=data_preparation.model_input(data_class,bkg_class)
     print(len(X.columns))
+
+    # RF
+    clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = clf.fit(X, y)
+
+    plot_helper.feature_rank(model,X)
+
     
 if __name__ == "__main__":
     start = time.time()
